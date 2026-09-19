0ed26fc21a54e3b6ddf8e8cf746949b1670e4d72
DevanshTomar <tomardevansh83@gmail.com>
2026-09-20 02:23:06 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Main {
+    public static String isExecutableInPath(String executableName) {
+        String pathEnv = System.getenv("PATH");
+        if (pathEnv == null) {
+            return null;
+        }
+
+        // Spliting by the system-dependent path separator (; on Windows, : on Unix)
+        return Stream.of(pathEnv.split(Pattern.quote(File.pathSeparator)))
+                .map(Paths::get)
+                .map(dir -> dir.resolve(executableName))
+                .filter(fullPath -> Files.isRegularFile(fullPath) && Files.isExecutable(fullPath))
+                .map(Path::toAbsolutePath)
+                .map(Path::toString)
+                .findFirst()
+                .orElse(null); // Returns null if no match is found
+    }
+
     public static void main(String[] args) throws Exception {
 
         HashSet<String> builtIn = new HashSet<>(Arrays.asList("echo", "exit", "type"));
@@ -28,7 +51,13 @@ public class Main {
                     if (builtIn.contains(commandToCheck)) {
                         System.out.println(commandToCheck + " is a shell builtin");
                     } else {
-                        System.out.println(commandToCheck+ ": not found");
+                        String executablePath = isExecutableInPath(commandToCheck);
+                        if (executablePath != null) {
+                            System.out.println(commandToCheck + " is " + executablePath);
+                        } else {
+                            System.out.println(commandToCheck+ ": not found");
+                        }
+
                     }
                 }
                 default -> System.out.println(userInput[0] + ": command not found");
