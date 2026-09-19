f2df9c74c5d6f276d25c0e69094d347f903f1f68
DevanshTomar <tomardevansh83@gmail.com>
2026-09-20 01:58:59 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         while (true) {
-            String userInput = IO.readln("$ ").trim();
+            String[] userInput = IO.readln("$ ").trim().split(" ");
+            if (userInput.length == 0) continue;
 
-            switch (userInput) {
+            String command = userInput[0];
+
+            switch (command) {
                 case "exit" -> System.exit(0);
-                default -> System.out.println(userInput + ": command not found");
+                case "echo" -> {
+                    if (userInput.length == 1) {
+                        continue;
+                    }
+                    String[] strings = Arrays.copyOfRange(userInput, 1, userInput.length);
+                    System.out.println(String.join(" ", strings));
+                }
+                default -> System.out.println(userInput[0] + ": command not found");
             }
         }
     }
