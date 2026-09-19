2183ece7448b6034ab5f485916bfba4c576c028c
DevanshTomar <tomardevansh83@gmail.com>
2026-09-20 01:35:50 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        String userInput = IO.readln("$ ");
+        System.out.println(userInput);
     }
 }
