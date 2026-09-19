c3117c569e94db1837a8fe107e4e4268e76315aa
DevanshTomar <tomardevansh83@gmail.com>
2026-09-20 02:08:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        HashSet<String> builtIn = new HashSet<>(Arrays.asList("echo", "exit", "Type"));
+        HashSet<String> builtIn = new HashSet<>(Arrays.asList("echo", "exit", "type"));
 
         while (true) {
             String[] userInput = IO.readln("$ ").trim().split(" ");
