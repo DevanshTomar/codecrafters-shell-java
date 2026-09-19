6d54f389e78086b55927f8c6a7ddc4011c8c1b7c
DevanshTomar <tomardevansh83@gmail.com>
2026-09-20 01:36:44 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         String userInput = IO.readln("$ ");
-        System.out.println(userInput);
+        System.out.println(userInput + ": command not found");
     }
 }
